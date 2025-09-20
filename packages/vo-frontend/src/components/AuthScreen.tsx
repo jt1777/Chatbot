7eca7e9fcf7eb5590fa200825f941c6f25ce735b
Jimmy Thai <jthai7@gmail.com>
2025-09-21 00:14:25 +0800

    last commit before trying to upload to testflight

--- packages/vo-frontend/src/components/AuthScreen.tsx
+++ packages/vo-frontend/src/components/AuthScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import axios from 'axios';
 import Toast from 'react-native-toast-message';
